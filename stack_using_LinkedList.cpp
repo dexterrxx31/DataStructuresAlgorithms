fd5a98e45493f52a4510ada946cc90753d832f17
Riyan Ahmad <riyanahmad99@gmail.com>
2021-05-09 23:38:10 +0530

    Stack linkedlist 2

--- stack_using_LinkedList.cpp
+++ stack_using_LinkedList.cpp
@@ -1,57 +1,87 @@
-#include<iostream>
-using namespace std ;
+#include <iostream>
+using namespace std;
 
-class Node {
-    public:
+class Node
+{
+public:
     int data;
-    class Node *next ;
+    class Node *next;
 };
 int isEmpty(class Node *ptr)
 {
-    if(ptr == NULL)
+    if (ptr == NULL)
     {
-        cout<<"Stack is Full";
+        //cout<<"Stack is Full";
         return 1;
-    }else{
-        return 0 ;
+    }
+    else
+    {
+        return 0;
     }
 }
 int isFull(class Node *ptr)
 {
-    class Node *newNode = (class Node*)malloc(sizeof(class Node));
+    class Node *newNode = (class Node *)malloc(sizeof(class Node));
     if (newNode == NULL)
     {
-        cout<<"Stack is Full";
+        //cout<<"Stack is Full";
         return 1;
-    }else {
+    }
+    else
+    {
         return 0;
     }
 }
-void push( class Node *top , int value)
+class Node *push(class Node *top, int value)
 {
-    class Node *newNode = (class Node *)malloc(sizeof(class Node));
-    if(newNode == NULL)
+    if (isFull(top))
     {
-        cout<<"Stack Overflow";
-    }else{
-        newNode->next = top ;
-        top = newNode ;
+        cout << "Stack Overflow" << endl;
+    }
+    else
+    {
+        class Node *newNode = (class Node *)malloc(sizeof(class Node));
+        newNode->data = value;
+        newNode->next = top;
+        top = newNode;
+        return top;
     }
 }
-int pop( class Node *top)
-{  
-    if(isEmpty(top)){
-        cout<<"Stack Underflow";
-        return -1 ;
-    }else{
-        class Node *temp = top ;
-        top = top->next;
-        int data = temp->data ;
+void linkedListTraversal(struct Node *ptr)
+{
+    while (ptr != NULL)
+    {
+        printf("Element: %d\n", ptr->data);
+        ptr = ptr->next;
+    }
+}
+int pop(class Node **top)
+{
+    if (isEmpty(*top))
+    {
+        cout << "Stack Underflow";
+        return -1;
+    }
+    else
+    {
+        class Node *temp = *top;
+        *top = (*top)->next;
+        int data = temp->data;
         free(temp);
-        return data ;
+        return data;
     }
 }
 int main()
 {
-    return 0 ;
+    class Node *top = NULL;
+    top = push(top, 78);
+    top = push(top, 7);
+    top = push(top, 8);
+
+    linkedListTraversal(top);
+    int element = pop(&top);
+    printf("Popped element is %d\n", element);
+    linkedListTraversal(top);
+
+    return 0;
 }
