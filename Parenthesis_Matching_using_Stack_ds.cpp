340ed8ad0df63d6ae9bb916b6a4ce236b4aa4181
Riyan Ahmad <riyanahmad99@gmail.com>
2021-05-14 00:01:08 +0530

    Parenthesis Matching Fixed

--- Parenthesis_Matching_using_Stack_ds.cpp
+++ Parenthesis_Matching_using_Stack_ds.cpp
@@ -1,54 +1,108 @@
 #include <iostream>
-#include <string.h>
+#include <stdlib.h>
 
 using namespace std;
 
 class Node
 {
 public:
-    int size ;
-    int top ;
-    class *arr;
+    int size;
+    int top;
+    char *arr;
 };
-
-class Node *top = NULL;
-//(class Node *)malloc(sizeof(class Node));
+int isFull(class Node *ptr)
+{
+    if (ptr->top == ptr->size)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
 int isEmpty(class Node *ptr)
 {
-    if (ptr == NULL)
-        return 1;
-    else
-        return 0;
-}
-
-class Node *push(class Node *ptr, char value)
-{
-    class Node *temp = (class Node *)malloc(sizeof(class Node));
-    temp->data = value;
-    temp->next = ptr;
-    ptr = temp;
-    return ptr;
-}
-
-void pop(class Node *ptr)
-{
-    class Node *temp = ptr;
-    top = (ptr)->next;
-    free(temp);
-}
-
-void Traversal(class Node *ptr)
-{
-    while (ptr != NULL)
+    if (ptr->top == -1)
     {
-        cout << ptr->data << endl;
-        ptr = ptr->next;
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+void push(class Node *ptr, char value)
+{
+    if (isFull(ptr))
+    {
+        cout << "Stack Overflow ! Sorry cant push further elements in Stack" << endl;
+    }
+    else
+    {
+        ptr->top++;
+        ptr->arr[ptr -> top] = value;
+    }
+}
+char pop(class Node *ptr)
+{
+    if (isFull(ptr))
+    {
+        cout << "Stack Underflow!";
+    }
+    else
+    {
+        char element = ptr->arr[ptr->top];
+        ptr->top--;
+        return element;
+    }
+}
+
+// void Traversal(class Node *ptr)
+// {
+//     while (ptr != NULL)
+//     {
+//         cout << ptr->data << endl;
+//         ptr = ptr->next;
+//     }
+// }
+
+int parenthesisMatching(char *expression)
+{
+    class Node *sp = (class Node *)malloc(sizeof(class Node));
+    sp->size = 20;
+    sp->top = -1;
+    sp->arr = (char *)malloc(sp->size * sizeof(char));
+
+    for (int i = 0; expression[i] != '\0'; i++)
+    {
+        if (expression[i] == '(')
+        {
+            push(sp, '(');
+        }
+        else if (expression[i] == ')')
+        {
+            if (isEmpty(sp))
+            {
+                return 0;
+            }
+            pop(sp);
+        }
     }
 }
 int main()
 {
-   char * expression = "((1+2)(*---$$)))";
+    const char * expression = "((8)(*--$$9))";
 
-   
+    if (parenthesisMatching(expression))
+    {
+        cout << "The parenthesis is matching";
+    }
+    else
+    {
+        cout << "The parenthesis is not matching";
+    }
+
     return 0;
 }
