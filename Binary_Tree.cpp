bd58f2af75aad4aee00b5ccb679cb27b6e62a627
Riyan Ahmad <riyanahmad99@gmail.com>
2021-05-18 02:39:22 +0530

    Theory about trees

---
+++ Binary_Tree.cpp
@@ -0,0 +1,14 @@
+/* Every node has atmost degree 2 is called binary tree
+if tree has n nodes then it will have n-1 edges
+Degree of node: No of direct children.
+Degree of tree : highest degree of a node among all the nodes present in the tree.
+Binary tree : Tree of degree two.
+*/
+#include<iostream>
+using namespace std;
+
+int main()
+{   
+    cout<<""<<endl;
+    return 0 ;
+}
