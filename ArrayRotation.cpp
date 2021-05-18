f6111504cc752131afee4b80d0fa5ad314486317
Riyan Ahmad <riyanahmad99@gmail.com>
2021-05-18 16:54:46 +0530

    C++ Vectors Array Rotation (interview Bit)

---
+++ ArrayRotation.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> rotateArray(vector<int> &A, int B)
+{
+    vector<int> ret;
+    if (B > A.size())
+    {
+        B = B % A.size();
+    }
+    for (int i = B; i < A.size(); i++)
+    {
+        ret.push_back(A[i]);
+    }
+    for (int j = 0; j < B; j++)
+    {
+        ret.push_back(A[j]);
+    }
+    return ret;
+}
+
+int main()
+{
+    vector<int> A;
+    int B = 10;
+    for (int i = 0; i < 6; i++)
+    {
+        A.push_back(i);
+    }
+
+    for (int j = 0; j < A.size(); j++)
+    {
+        cout << A[j] << "|";
+    }
+    cout << endl;
+    vector<int> C = rotateArray(A, B);
+
+    for (int j = 0; j < C.size(); j++)
+    {
+        cout << C[j] << "|";
+    }
+
+    return 0;
+}
