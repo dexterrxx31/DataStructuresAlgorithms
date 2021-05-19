6d7e55003968f05f962375767cea8483c861e22e
Riyan Ahmad <riyanahmad99@gmail.com>
2021-05-19 13:11:14 +0530

    Creates Binary Tree and linked it

--- Binary_Tree.cpp
+++ Binary_Tree.cpp
@@ -7,8 +7,32 @@ Binary tree : Tree of degree two.
 #include<iostream>
 using namespace std;
 
+class Node {
+    public:
+    int data;
+    class Node* left ;
+    class Node* right ;
+};
+
+class Node* create_Node (int data)
+{
+    class Node* newNode = (class Node*)malloc(sizeof(class Node));
+    newNode->data = data ;
+    newNode->left = NULL;
+    newNode->right = NULL ;
+    return newNode;
+}
 int main()
 {   
-    cout<<""<<endl;
+    class Node * p = create_Node(1);
+    class Node * p1 = create_Node(2);
+    class Node * p2 = create_Node(3);
+    //class Node * p3 = create_Node(4);
+    //class Node * p4 = create_Node(5);
+
+    //Linking Nodes to form a tree 
+
+    p->left = p1 ;
+    p->right = p2 ;
     return 0 ;
 }
