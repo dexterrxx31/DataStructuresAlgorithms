daee526fc7dc7040e4111a4c74281043bbe085e3
Riyan Ahmad <riyanahmad99@gmail.com>
2021-06-16 20:45:38 +0530

    initial commit

---
+++ rain.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+
+int rain_water_collected(vector<int> arr)
+{
+    int result ;
+    
+    return result;
+}
+
+int main()
+{   
+    vector<int> arr = {0,1,0,2,1,0,1,3,2,1,2,1};
+    int rain_water = rain_water_collected(arr);
+    cout<<"Total rain water collected :"<<rain_water;
+    return 0 ;
+}
