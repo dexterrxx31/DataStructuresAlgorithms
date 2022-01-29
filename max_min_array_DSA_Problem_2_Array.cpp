e3cf8d9a57d7868e80b7dc704959aaeb93f3c09c
Riyan Ahmad <riyanahmad99@gmail.com>
2022-01-29 21:35:55 +0530

    Max Min using Tournament

--- max_min_array_DSA_Problem_2_Array.cpp
+++ max_min_array_DSA_Problem_2_Array.cpp
@@ -1,16 +1,16 @@
 #include<iostream>
 using namespace std;
 
-
+//Pair Structure
 struct Pair {
     int max ;
     int min ;
 };
 
-Pair max_min(int arr[])
+// Simple Linear Search Method
+Pair max_min_linear_search(int arr[] , int size)
 {   
     struct Pair result ;
-    //int size = sizeof(arr) / sizeof(arr[0]);
     if (arr[0] > arr[1])
     {
         result.max = arr[0];
@@ -22,7 +22,7 @@ Pair max_min(int arr[])
         result.min = arr[0];        
     }
     
-    for (int i = 2 ; i < 10 ; i ++)
+    for (int i = 2 ; i < size ; i ++)
     {
         if(arr[i] > result.max)
         {
@@ -35,10 +35,61 @@ Pair max_min(int arr[])
     }
     return result ; 
 }
+
+//Tournament Approch
+Pair max_min_tournament(int arr[] , int low , int high)
+{   
+    struct Pair result , mml , mmr ;
+    int mid ;
+
+    if(low == high )
+    {
+        result.max = arr[0];
+        result.min = arr[0];
+        return result;
+    }
+
+    if (high == low + 1)
+    {
+        if (arr[low] > arr[high])
+        {
+            result.max = arr[low];
+            result.min = arr[high];
+        }
+        else
+        {
+            result.max = arr[high];
+            result.min = arr[low];
+        }
+        return result;
+    }
+    
+    mid = (low + high) / 2 ;
+    mml = max_min_tournament(arr , low , mid);
+    mmr = max_min_tournament(arr , mid+1 , high);
+    
+    if (mml.min < mmr.min)
+        result.min = mml.min;
+    else
+        result.min = mmr.min;    
+     
+    if (mml.max > mmr.max)
+        result.max = mml.max;
+    else
+        result.max = mmr.max;  
+
+    return result ; 
+}
+
+
+
+//Driver program
 int main()
 {   
     int arr[] = {2,1,0,67,-99,8,5,7, 8 , 90};
-    struct Pair result = max_min(arr);
+    int size = sizeof(arr)/sizeof(arr[0]);
+    //cout<<"::"<<size<<"::"<<endl;
+    struct Pair result = max_min_tournament(arr , 0 , size - 1 );
     cout<<"Max number is :"<<result.max<<endl;
     cout<<"Min number is :"<<result.min<<endl;
     return 0 ; 
