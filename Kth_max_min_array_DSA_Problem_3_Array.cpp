b199c48c2afa942a710d89037ef7a3a03c383f4b
Riyan Ahmad <riyanahmad99@gmail.com>
2022-01-30 16:40:32 +0530

    updated

--- Kth_max_min_array_DSA_Problem_3_Array.cpp
+++ Kth_max_min_array_DSA_Problem_3_Array.cpp
@@ -9,12 +9,9 @@ int main()
     int arr[] = {2,3,0,44,-1,6,4,100,-99};
     int size = sizeof(arr) / sizeof(arr[0]);
     int k = 4;
- 
-    set<int> s(arr, arr + n);
+    set<int> s(arr, arr + size); //declaring set and putting array elements inside
     set<int>::iterator itr = s.begin(); // s.begin() returns a pointer to first
-  
     advance(itr, k - 1); // itr points to kth element in set
- 
     cout << *itr << "\n";
     return 0;
 }
