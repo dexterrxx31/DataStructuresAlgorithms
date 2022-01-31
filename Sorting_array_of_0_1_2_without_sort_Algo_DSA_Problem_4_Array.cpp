96de70654130ae484c109fcb5485964b51dcd585
Riyan Ahmad <riyanahmad99@gmail.com>
2022-01-31 20:21:07 +0530

    DNF sort added

--- Sorting_array_of_0_1_2_without_sort_Algo_DSA_Problem_4_Array.cpp
+++ Sorting_array_of_0_1_2_without_sort_Algo_DSA_Problem_4_Array.cpp
@@ -3,6 +3,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//Swap elements in an array by index
+void swap(int arr[], int n, int m)
+{
+    int temp;
+    temp = arr[n];
+    arr[n] = arr[m];
+    arr[m] = temp;
+}
+
 //Using STL Multiset
 void multiset_method(int arr[] , int size)
 {
@@ -13,10 +22,42 @@ void multiset_method(int arr[] , int size)
     }
 }
 
+void dnf_sort(int arr[] , int  size)
+{
+    int lo = 0 ;
+    int mid = 0 ;
+    int hi = size - 1 ;
+
+    while(mid <= hi )
+    {
+        if(arr[mid] == 0)
+        {
+            swap(arr , mid ,lo);
+            mid++;
+            lo++;
+        }
+        else if(arr[mid] ==1)
+        {
+            mid++;
+        }
+        else
+        {   
+            swap(arr , mid , hi);
+            hi--;
+        }
+        
+    }
+}
 int main()
 {
     int arr[] = {1,2,0,0,0,2,2,1,1,0};
     int size = sizeof(arr) / sizeof(arr[0]);
-    multiset_method(arr,size);
+    //multiset_method(arr,size);
+    dnf_sort(arr,size);
+    for (int i = 0; i < size; i++)
+    {
+        cout<<arr[i] <<" ";
+    }
+    
     return 0 ;
 }
