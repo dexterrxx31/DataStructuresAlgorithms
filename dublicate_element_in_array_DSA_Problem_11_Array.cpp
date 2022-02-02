96b2e99f61f35e115b6ee7311772cb08b098f18d
Riyan Ahmad <riyanahmad99@gmail.com>
2022-02-02 15:20:52 +0530

    By sorting Array (Rectified)

--- dublicate_element_in_array_DSA_Problem_11_Array.cpp
+++ dublicate_element_in_array_DSA_Problem_11_Array.cpp
@@ -1,22 +1,25 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int dublicate_sort(int arr[] , int n)
+int dublicate_sort(int arr[], int n)
 {
-    sort(arr , arr + n);
+    sort(arr, arr + n);
     for (int i = 0; i < n - 1; i++)
     {
-        if(arr[i] == arr[i+1])
-            cout<<arr[i];
+        if (arr[i] == arr[i + 1])
+        {
+            cout << arr[i];
+            break;
+        }
     }
 }
 
 int main()
-{   
-    int arr[] = {1,3,4,2,2};
+{
+    int arr[] = {1, 3, 4, 2, 2};
     int n = sizeof(arr) / sizeof(arr[0]);
     //cout<<binary_search(arr , 1 , 0  , 4);
     //cout<<dublicate(arr ,n);
-    dublicate_sort(arr , n);
-    return 0 ;
+    dublicate_sort(arr, n);
+    return 0;
 }
