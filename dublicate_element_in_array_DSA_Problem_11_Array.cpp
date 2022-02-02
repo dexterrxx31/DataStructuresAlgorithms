164e1eaf2560433176086a6972e904beb18e0b92
Riyan Ahmad <riyanahmad99@gmail.com>
2022-02-02 15:36:47 +0530

    Brute Force approch

--- dublicate_element_in_array_DSA_Problem_11_Array.cpp
+++ dublicate_element_in_array_DSA_Problem_11_Array.cpp
@@ -1,6 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int brute_force_approch(int arr[], int n)
+{
+    int count;
+    for (int i = 0; i < n; i++)
+    {
+        count = 0;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[i] == arr[j])
+            {
+                count = 1;
+            }
+        }
+        if (count == 1)
+        {
+            return arr[i];
+        }
+    }
+}
+
 int dublicate_sort(int arr[], int n)
 {
     sort(arr, arr + n);
@@ -8,8 +28,7 @@ int dublicate_sort(int arr[], int n)
     {
         if (arr[i] == arr[i + 1])
         {
-            cout << arr[i];
-            break;
+            return arr[i];
         }
     }
 }
@@ -18,8 +37,7 @@ int main()
 {
     int arr[] = {1, 3, 4, 2, 2};
     int n = sizeof(arr) / sizeof(arr[0]);
-    //cout<<binary_search(arr , 1 , 0  , 4);
-    //cout<<dublicate(arr ,n);
-    dublicate_sort(arr, n);
+    // cout<<dublicate_sort(arr, n);
+    // cout<<brute_force_approch(arr,n);
     return 0;
 }
