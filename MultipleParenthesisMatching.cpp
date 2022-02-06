15ca42ca5e2d4ac0d3651b3ffea6bfee5a8f655b
Riyan Ahmad <riyanahmad99@gmail.com>
2022-02-06 21:21:41 +0530

    Updated

--- MultipleParenthesisMatching.cpp
+++ MultipleParenthesisMatching.cpp
@@ -1,49 +1,41 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Node
+struct Node
 {
-public:
     int size;
     int top;
     char *arr;
 };
-int isFull(class Node *ptr)
+
+int isFull(Node *ptr)
 {
     if (ptr->top == ptr->size - 1)
-    {
         return 1;
-    }
     else
-    {
         return 0;
-    }
-}
-int isEmpty(class Node *ptr)
-{
-    if (ptr->top == -1)
-    {
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
 }
 
-void push(class Node *ptr, char value)
+int isEmpty(Node *ptr)
+{
+    if (ptr->top == -1)
+        return 1;
+    else
+        return 0;
+}
+
+void push(Node *ptr, char value)
 {
     if (isFull(ptr))
-    {
-        cout << "Stack Overflow ! Sorry cant push further elements in Stack" << endl;
-    }
+        cout << "Stack Overflow !!!" << endl;
     else
     {
         ptr->top++;
         ptr->arr[ptr->top] = value;
     }
 }
-char pop(class Node *ptr)
+
+char pop(Node *ptr)
 {
     if (isEmpty(ptr))
     {
@@ -58,66 +50,50 @@ char pop(class Node *ptr)
     }
 }
 
-int matched(char a, char b){
-    if(a=='{' && b=='}'){
+int matched(char a, char b)
+{
+    if (a == '{' && b == '}')
         return 1;
-    }
-    if(a=='(' && b==')'){
+    if (a == '(' && b == ')')
         return 1;
-    }
-    if(a=='[' && b==']'){
+    if (a == '[' && b == ']')
         return 1;
-    }
-  return 0;  
+    return 0;
 }
 
 int parenthesisMatching(const char *expression)
 {
-    class Node *sp = (class Node *)malloc(sizeof(class Node));
+    Node *sp = new Node;
     sp->size = 20;
     sp->top = -1;
-    sp->arr = (char *)malloc(sp->size * sizeof(char));
+    sp->arr = new char[sp->size];
 
     for (int i = 0; expression[i] != '\0'; i++)
     {
-        if (expression[i] == '(' || expression[i] == '{' || expression[i] == '[' )
-        {
+        if (expression[i] == '(' || expression[i] == '{' || expression[i] == '[')
             push(sp, expression[i]);
-        }
-        else if (expression[i] == ')' || expression[i] == '}' || expression[i] == ']' )
+        else if (expression[i] == ')' || expression[i] == '}' || expression[i] == ']')
         {
             if (isEmpty(sp))
-            {
                 return 0;
-            }
-            char popped_element =  pop(sp);
-            if(!matched(popped_element , expression[i]))
-            {
-                return 0 ;
-            }
+            char popped_element = pop(sp);
+            if (!matched(popped_element, expression[i]))
+                return 0;
         }
     }
+
     if (isEmpty(sp))
-    {
         return 1;
-    }
     else
-    {
         return 0;
-    }
 }
+
 int main()
-{   // it is necessary to have const char pointer else code will not work as per as c++
+{ // it is necessary to have const char pointer else code will not work as per as c++
     const char *expression = "((8)(*--$$9))";
-
     if (parenthesisMatching(expression))
-    {
         cout << "The parenthesis is matching";
-    }
     else
-    {
         cout << "The parenthesis is not matching";
-    }
-
     return 0;
 }
