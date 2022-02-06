a3af26e2057c70fa59dfc0d5b9e949c4656d5d4c
Riyan Ahmad <riyanahmad99@gmail.com>
2022-02-07 00:25:16 +0530

    Dequeue

--- queue.cpp
+++ queue.cpp
@@ -1,23 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct queue
+struct Queue
 {
     int size;
     int front;
     int rear;
-    int arr *
+    int *arr;
 };
 
-int isFull(queue *ptr)
+int isFull(Queue *ptr)
 {
-    if (ptr > rear == ptr->size - 1)
+    if (ptr->rear == ptr->size - 1)
         return 1;
     else
         return 0;
 }
 
-int isEmpty(queue *ptr)
+int isEmpty(Queue *ptr)
 {
     if (ptr->front == ptr->rear)
         return 1;
@@ -25,20 +25,41 @@ int isEmpty(queue *ptr)
         return 0;
 }
 
-void enqueue(queue *ptr, int val)
-{   
-    if(isFull)
-        cout<<"Queue is Full"<<endl;
+void enqueue(Queue *ptr, int val)
+{
+    if (isFull(ptr))
+        cout << "Queue is Full" << endl;
     else
     {
         ptr->rear++;
-        ptr->arr[rear] = val;
+        ptr->arr[ptr->rear] = val;
+    }
+}
+
+int dequeue(Queue *ptr)
+{
+    if (isEmpty(ptr))
+        cout << "Queue is Empty" << endl;
+    else
+    {
+        ptr->front++;
+        int deleted_item = ptr->arr[ptr->front];
+        ptr->arr[ptr->front] = 0;
+        return deleted_item;
+    }
+}
+
+void traversal(Queue *ptr)
+{
+    for (int i = 0; i < ptr->size; i++)
+    {
+        cout << ptr->arr[i] << " ";
     }
 }
 
 int main()
 {
-    queue *st = new queue;
+    Queue *st = new Queue;
     st->size = 20;
     st->front = -1;
     st->rear = -1;
@@ -46,10 +67,15 @@ int main()
 
     int input_arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
     int n = sizeof(input_arr) / sizeof(input_arr[0]);
+
     for (int i = 0; i < n; i++)
     {
-        push(st, input_arr[i]);
+        enqueue(st, input_arr[i]);
     }
 
+    traversal(st);
+    cout << endl
+         << dequeue(st) << endl;
+    traversal(st);
     return 0;
 }
