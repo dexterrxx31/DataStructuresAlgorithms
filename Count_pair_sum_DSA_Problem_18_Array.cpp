6f118ebe97a760eb65a43aedd0e8fb7c31cd424d
Riyan Ahmad <riyanahmad99@gmail.com>
2022-02-11 22:20:56 +0530

    Hashmap

--- Count_pair_sum_DSA_Problem_18_Array.cpp
+++ Count_pair_sum_DSA_Problem_18_Array.cpp
@@ -1,26 +1,29 @@
 // https://practice.geeksforgeeks.org/problems/count-pairs-with-given-sum5022/1
 
 #include <bits/stdc++.h>
-#include <unordered_set>
 using namespace std;
 
-// int getPairsCount(int arr[], int n, int k)
-// {   
-//     int count = 0 ;
-//     for (int i = 0; i < n; i++)
-//     {
-//         int required_number = k - arr[i];
-        
-//     }
-//     return count ;
-// }
+int getPairsCount(int arr[], int n, int k)
+{
+    int count = 0;
+    unordered_map<int, int> mp;
+    for (int i = 0; i < n; i++)
+        mp[arr[i]]++;
+
+    for (int i = 0; i < n; i++)
+        if (arr[i] == k - arr[i])
+            count += (mp[arr[i]] - 1);
+        else
+            count += mp[k - arr[i]];
+
+    return count;
+}
 
 int main()
 {
     int arr[] = {1, 5, 7, 1};             // input array
     int n = sizeof(arr) / sizeof(arr[0]); // size of an array
-    //int k = 6;                            // sum we need
-    cout<<mp[9] ;
-    //cout << getPairsCount(arr, n, k);
+    int k = 6;                            // sum we need
+    cout << getPairsCount(arr, n, k);
     return 0;
 }
