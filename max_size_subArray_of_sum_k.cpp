257fcb24722b3bee2b63e2b715590b5b00a5ba5c
Riyan Ahmad <riyanahmad99@gmail.com>
2022-02-13 00:52:16 +0530

    Sliding Window

---
+++ max_size_subArray_of_sum_k.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> arr{4, 1, 1, 1, 2, 3, 5}; // input String
+    int n = arr.size();                   // size of input Array
+
+    int k = 5; // sum
+    int sum = 0;
+    int max_sum = INT_MIN;
+    int i, j;  // Begin and end Pointers
+    i = j = 0; // Initial State of Pointers
+
+    // Sliding window Loop
+    while (j < n)
+    {
+        sum = sum + arr[j];
+        if (sum < k)
+            j++;
+
+        else if (sum == k)
+        {
+            max_sum = max(max_sum, j - i + 1);
+            j++;
+        }
+        else if (sum > k)
+        {
+            while (sum > k)
+            {
+                sum = sum - arr[i];
+                i++;
+            }
+            j++;
+        }
+    }
+    cout << max_sum;
+}
