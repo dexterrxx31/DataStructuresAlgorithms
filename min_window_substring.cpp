db5c402be5b540674d2a4731054cc121facd1a2f
Riyan Ahmad <riyanahmad99@gmail.com>
2022-02-22 15:33:07 +0530

    Sliding Window

--- min_window_substring.cpp
+++ min_window_substring.cpp
@@ -3,16 +3,16 @@ using namespace std;
 
 int main()
 {
-    string s = "";               // input string ;
-    string pattern = "";         // pattern string
+    string s = "ADOBECODEBANC";  // input string ;
+    string pattern = "BANC";     // pattern string
     unordered_map<char, int> mp; // map to store the frequency of each char in pattern string
     int i, j, count, min_length; // declaring variables
     i = j = 0;                   // initial value of both the window pointers
     min_length = INT_MAX;        // minimum length of window
-
+    int start, end;              // to output the substring
     // Loop to create the map of frequency of each letter in pattern string
-    for (int i = 0; i < s.size(); i++)
-        mp[s[i]]++;
+    for (int i = 0; i < pattern.size(); i++)
+        mp[pattern[i]]++;
 
     count = mp.size(); // number of unique characters available in pattern string
 
@@ -26,27 +26,37 @@ int main()
                 mp[s[j]]--;
                 if (mp[s[j]] == 0)
                     count--;
-                j++;
-            }
-            else 
-            {
-                j++;
             }
         }
-        else if (count == 0)
+
+        if (count == 0)
         {
-            while(count == 0)
+            // Loop to reduce the window size to minimum.
+            while (count == 0)
             {
-                if(mp[s[i]] < 0)
-                    mp[s[i]]++ ;
-                if(mp[s[i]] == 0)
-                    count++;
+                min_length = min(min_length, j - i + 1); // updating the minimum window size
+                // Keeping start and end pointers to keep updated the output string at last.
+                start = i;
+                end = j;
+
+                if (mp.find(s[i]) != mp.end())
+                {
+                    mp[s[i]]++;
+                    if (mp[s[i]] > 0)
+                        count++;
+                }
                 i++;
             }
-            min_length = min(min_length , j - i + 1);
-            j++;
         }
+        j++;
     }
-    cout<<min_length ;
+
+    cout << "Minimum length of window is : " << min_length << endl
+         << "Substring : ";
+    for (int i = start; i <= end; i++)
+    {
+        cout << s[i];
+    }
+
     return 0;
 }
