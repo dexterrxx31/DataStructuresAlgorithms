ef682b801081afa90d9321be1f1e0d4465c33d41
Riyan Ahmad <riyanahmad99@gmail.com>
2022-03-02 14:45:57 +0530

    Stack

--- nearestGrestestToLeft.cpp
+++ nearestGrestestToLeft.cpp
@@ -3,36 +3,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Brute force Approch O(n2)
-void nextGreatestElementWithoutStack(vector<int> arr)
-{
-    int n = arr.size();
-    for (int i = 0; i < n; i++)
-    {
-        bool flag = true;
-        for (int j = i + 1; j < n; j++)
-        {
-            if (arr[j] > arr[i])
-            {
-                flag = false;
-                cout << arr[i] << "::" << arr[j] << endl;
-                break;
-            }
-        }
-        if (flag)
-        {
-            cout << arr[i] << "::" << -1 << endl;
-        }
-    }
-}
-
 // Using Stack data structure
 vector<int> NGEWithStack(vector<int> arr)
 {
     int n = arr.size();
     stack<int> st;
     vector<int> ans;
-    for (int i = n - 1; i >= 0; i--)
+    for (int i = 0; i < n; i++)
     {
         if (!st.empty())
         {
@@ -44,14 +21,13 @@ vector<int> NGEWithStack(vector<int> arr)
         st.empty() ? ans.push_back(-1) : ans.push_back(st.top());
         st.push(arr[i]);
     }
-    reverse(ans.begin(), ans.end());
+
     return ans;
 }
 
 int main()
 {
     vector<int> arr{11, 13, 21, 3};
-    // nextGreatestElementWithoutStack(arr);
     vector<int> answer = NGEWithStack(arr);
     for (int i = 0; i < answer.size(); i++)
     {
