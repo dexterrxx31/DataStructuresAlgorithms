e2d7eb0469d8231c91c295640fa3cc2c6fa48a7e
Riyan Ahmad <riyanahmad99@gmail.com>
2022-03-18 00:43:42 +0530

    Stack

--- max_to_left_right_Stack.cpp
+++ max_to_left_right_Stack.cpp
@@ -10,7 +10,7 @@ vector<int> max_right(vector<int> arr)
     {
         if (s.size() == 0)
         {
-            v.push_back(arr[i]);
+            v.push_back(0);
             s.push(arr[i]);
             continue;
         }
@@ -37,7 +37,7 @@ vector<int> max_left(vector<int> arr)
     {
         if (s.size() == 0)
         {
-            v.push_back(arr[i]);
+            v.push_back(0);
             s.push(arr[i]);
             continue;
         }
@@ -54,23 +54,15 @@ vector<int> max_left(vector<int> arr)
     return v;
 }
 
-int total_water_trapped(vector<int> arr)
-{
-    int water = 0;
-    vector<int> rt = max_right(arr);
-    vector<int> lft = max_left(arr);
-    for (int i = 0; i < arr.size(); i++)
-    {
-        int height_of_water = min(rt[i], lft[i]);
-        water += height_of_water - arr[i];
-    }
-    return water;
-}
-
 int main()
 {
     vector<int> arr{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
     // vector<int> arr{3,0,2,0,4};
-    cout << total_water_trapped(arr);
+    vector<int> ans = max_left(arr);
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i] << " ";
+    }
+
     return 0;
 }
