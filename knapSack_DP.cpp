fb3ac4a492ce53282166f0650ca17a52f2e7dced
Riyan Ahmad <riyanahmad99@gmail.com>
2022-03-18 02:54:16 +0530

    DP

--- knapSack_DP.cpp
+++ knapSack_DP.cpp
@@ -18,12 +18,35 @@ int KnapSack_REC(int weight[], int profit[], int W, int n)
     }
 }
 
+//Memoization
+vector<vector<int> > t( 100 , vector<int> (100, -1));
+
+int KnapSack_DP(int weight[], int profit[], int W, int n)
+{
+    if (W == 0 || n == 0)
+    {
+        return 0;
+    }
+    if(t[n][W] != -1 )
+    {
+        return t[n][W];
+    }
+    if (weight[n - 1] <= W)
+    {
+        return t[n][W] = max(profit[n - 1] + KnapSack_DP(weight, profit, W - weight[n - 1], n - 1), KnapSack_DP(weight, profit, W, n - 1));
+    }
+    else if (weight[n - 1] > W)
+    {
+        return t[n][W] = KnapSack_DP(weight, profit, W, n - 1);
+    }
+}
+
 int main()
 {
     int profit[] = {60, 100, 120};
     int weight[] = {10, 20, 30};
     int W = 50;
     int n = 3;
-    cout << KnapSack_REC(weight, profit, W, n);
+    cout << KnapSack_DP(weight, profit, W, n);
     return 0;
 }
