1c5c812a09428b1a3d417948394a5e263843aad9
Riyan Ahmad <riyanahmad99@gmail.com>
2022-03-19 00:22:25 +0530

    DP

--- knapSack_DP.cpp
+++ knapSack_DP.cpp
@@ -18,8 +18,8 @@ int KnapSack_REC(int weight[], int profit[], int W, int n)
     }
 }
 
-//Memoization
-vector<vector<int> > t( 100 , vector<int> (100, -1));
+// Memoization
+vector<vector<int>> t(100, vector<int>(100, -1));
 
 int KnapSack_DP(int weight[], int profit[], int W, int n)
 {
@@ -27,7 +27,7 @@ int KnapSack_DP(int weight[], int profit[], int W, int n)
     {
         return 0;
     }
-    if(t[n][W] != -1 )
+    if (t[n][W] != -1)
     {
         return t[n][W];
     }
@@ -41,12 +41,46 @@ int KnapSack_DP(int weight[], int profit[], int W, int n)
     }
 }
 
+// Top Down Approch
+int KnapSack_DP2(int weight[], int profit[], int W, int n)
+{
+    int tt[n + 1][W + 1];
+
+    for (int i = 0; i < n + 1; i++)
+    {
+        for (int j = 0; j < W + 1; j++)
+        {
+            if (i == 0 || j == 0)
+            {
+                tt[i][j] = 0;
+            }
+        }
+    }
+
+    for (int i = 1; i < n + 1; i++)
+    {
+        for (int j = 1; j < W + 1; j++)
+        {
+            if (weight[i - 1] <= W)
+            {
+                tt[i][j] = max(profit[i - 1] + tt[i - 1][j - weight[i - 1]], tt[i - 1][j]);
+            }
+            else if (weight[i - 1] > W)
+            {
+                tt[i][j] = tt[i - 1][j];
+            }
+        }
+    }
+
+    return tt[n][W];
+}
+
 int main()
 {
     int profit[] = {60, 100, 120};
     int weight[] = {10, 20, 30};
     int W = 50;
     int n = 3;
-    cout << KnapSack_DP(weight, profit, W, n);
+    cout << KnapSack_DP2(weight, profit, W, n);
     return 0;
 }
