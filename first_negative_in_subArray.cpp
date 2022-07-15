867566ef53f9000d01a8e3ed3e9d53a8ba9d0f10
Riyan Ahmad <riyanahmad99@gmail.com>
2022-07-16 00:30:22 +0530

    Sliding Window

--- first_negative_in_subArray.cpp
+++ first_negative_in_subArray.cpp
@@ -42,3 +42,50 @@ int main()
 
     return 0;
 }
+
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+void fxn(int arr[], int k, int n)
+{
+    deque<int> neq;
+    int i = 0, j = 0;
+
+    while (j < n)
+    {
+        if (arr[j] < 0)
+        {
+            neq.push_back(arr[j]);
+        }
+        if (j - i + 1 < k)
+        {
+            j++;
+        }
+        else
+        {
+            if (neq.empty())
+                cout << 0;
+            else
+            {
+                cout << neq.front();
+
+                if (neq.front() == arr[i])
+                {
+                    neq.pop_front();
+                }
+            }
+            i++;
+            j++;
+        }
+    }
+}
+int main()
+{
+    int arr[] = {12, -1, -7, 8, -15, 30, 16, 28};
+    int K = 3;
+    int N = sizeof(arr) / sizeof(arr[0]);
+    fxn(arr, K, N);
+    return 0;
+}
+*/
