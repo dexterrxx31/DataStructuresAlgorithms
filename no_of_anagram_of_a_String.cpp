26ee25bcb665fc26d1dd47947f15340ca0360909
Riyan Ahmad <riyanahmad99@gmail.com>
2022-07-17 00:01:01 +0530

    Sliding Window

--- no_of_anagram_of_a_String.cpp
+++ no_of_anagram_of_a_String.cpp
@@ -53,3 +53,70 @@ int main()
     cout << ans;
     return 0;
 }
+
+
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+int fxn(string str, string ptt, int n, int k)
+{
+    unordered_map<char, int> mp;
+
+    for (int i = 0; i < k; i++)
+    {
+        mp[ptt[i]]++;
+    }
+    unordered_map<char, int>::iterator it;
+
+    for (it = mp.begin(); it != mp.end(); it++)
+    {
+        cout << it->first << "->" << it->second << endl;
+    }
+
+    int i = 0, j = 0;
+    int counter = mp.size();
+    int answer = 0;
+
+    while (j < n)
+    {
+        // initial calculation step
+        if (mp.find(str[j]) != mp.end())
+        {
+            mp[str[j]]--;
+            if (mp[str[j]] == 0)
+                counter--;
+        }
+
+        if (j - i + 1 < k)
+        {
+            j++;
+        }
+
+        else
+        {
+            if(counter == 0 )
+                answer++;
+            if(mp.find(str[j]) != mp.end())
+            {
+                mp[str[i]]++;
+                if(mp[str[i]] == 1)
+                    counter++;
+            }
+            i++;
+            j++;
+        }
+    }
+    return answer ;
+}
+
+int main()
+{
+    string arr_in = "forfoforfro"; // input String
+    int n = arr_in.size();         // size of input String
+    string arr_ptr = "for";        // Pattern String
+    int k = arr_ptr.size();        // size of window
+    cout<<fxn(arr_in , arr_ptr , n , k );
+    return 0;
+}
+*/
