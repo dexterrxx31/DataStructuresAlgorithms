650aba6bbdb563f11dda1335c4a6742282b18b14
Riyan Ahmad <riyanahmad99@gmail.com>
2022-07-20 02:13:30 +0530

    BST Tree Traversal

--- BST_traversals.cpp
+++ BST_traversals.cpp
@@ -39,10 +39,33 @@ void inorder_traversal(Node *root)
     }
 }
 
+void preorder_traversal(Node *root)
+{
+    if (root != NULL)
+    {   cout<<root->data<<" ";
+        inorder_traversal(root->left);
+        inorder_traversal(root->right);
+    }
+}
+
+void postorder_traversal(Node *root)
+{
+    if (root != NULL)
+    {
+        inorder_traversal(root->left);
+        inorder_traversal(root->right);
+        cout<<root->data<<" ";
+    }
+}
+
 int main()
 {
     int arr[] = {1, 2, 3, 4, 5, 6, 6, 6, 6};
     int n = sizeof(arr) / sizeof(arr[0]);
     Node *root = insertLevelOrder(arr, 0, n);
     inorder_traversal(root) ;
+    cout<<"\n";
+    preorder_traversal(root);
+    cout<<"\n";
+    postorder_traversal(root) ;
 }
