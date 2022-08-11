367c6f3923a21090859ebaebce40199087f84faf
Riyan <riyanahmad99@gmail.com>
2022-08-11 17:19:18 +0530

    Cutting Rod Recursion

--- cutting_rod.cpp
+++ cutting_rod.cpp
@@ -1,24 +1,22 @@
-#include<bits/stdc++.h>
-using namespace std ;
+#include <bits/stdc++.h>
+using namespace std;
 
-//Top Down Approch
-vector<vector<int>> t(9 , vector<int>(9 , -1)) ;
-
-int rod(int W ,int n , vector<int> prices , vector<int> length )
+// Recursion Approch
+int rod(vector<int> prices, int n)
 {
-    if(n == 0 || W == 0 ) return 0 ; 
-
-    if(t[n][W] != -1) return t[n][W] ;
-
-    if(length[n - 1 ] <= W)
-    return t[n][W] = max(prices[n-1] + rod(W-length[n-1] , n-1 , prices , length) , rod(W , n-1 , prices , length));
-    else if(length[n-1] > W)
-    return  t[n][W] = rod(W , n-1 , prices , length);
-    return -1 ;
+    if (n <= 0) return 0;
+    int ans = INT_MIN;
+    for (int i = 0; i < n; i++)
+    {
+        int cut = i + 1;
+        int current_ans = prices[i] + rod(prices, n - cut);
+        ans = max(ans, current_ans);
+    }
+    return ans;
 }
 
 int main()
-{   
-    cout<<rod(8 , 8 , {6,5,8,9,10,17,17,20} ,{1,2,3,4,5,6,7,8});
-    return 0 ; 
+{
+    cout << rod({3, 5, 8, 9, 10, 17, 17, 20}, 8);
+    return 0;
 }
