bd43a9433d7706c2e14d733edb359762a8a00726
Riyan <riyanahmad99@gmail.com>
2022-08-11 19:26:08 +0530

    DP

--- cutting_rod.cpp
+++ cutting_rod.cpp
@@ -1,11 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Recursion Approch
+//Top Down Memoization
+
 int rod(vector<int> prices, int n)
 {
     if (n <= 0) return 0;
     int ans = INT_MIN;
+
     for (int i = 0; i < n; i++)
     {
         int cut = i + 1;
@@ -13,6 +15,45 @@ int rod(vector<int> prices, int n)
         ans = max(ans, current_ans);
     }
     return ans;
+}
+
+//Top Down Approch
+
+vector<int> t(9 , -1) ;
+int rod(vector<int> prices, int n)
+{
+    if (n <= 0) return 0;
+    if(t[n-1] != -1) return t[n-1] ;
+
+    int ans = INT_MIN;
+
+    for (int i = 0; i < n; i++)
+    {
+        int cut = i + 1;
+        int current_ans = prices[i] + rod(prices, n - cut);
+        ans = max(ans, current_ans);
+    }
+    return t[n-1] = ans;
+}
+
+//Bottom Up Approch
+
+int rod(vector<int> prices, int n)
+{
+    vector<int> t(n+1, 0);
+
+    for (int j = 1; j <= n; j++)
+    {
+        int ans = INT_MIN;
+        for(int i = 0 ; i < j ; i++)
+        {
+            int cut = i+1 ; 
+            int  current_ans = prices[i] + t[j-cut] ;
+            ans = max( ans , current_ans) ;
+        }
+        t[j] = ans ;
+    }
+    return t[n];
 }
 
 int main()
